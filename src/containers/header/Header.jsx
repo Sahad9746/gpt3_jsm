e7c54557f9df8bfb1a3e1d93b0c941bed68dcdec
SaHaD_A-T <sahadali696@gmail.com>
2023-07-19 16:38:45 +0530

    homw section structuring is completed

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import './header.css';
+import People from "../../assets/people.png"
+import Ai from "../../assets/ai.png"
 
-function Header() {
+
+
+const Header = () => {
   return (
-    <div>
-      Header
+    <div className='gpt3__header section_padding' id="home">
+      <div className='gpt3__header-container'>
+        <h1 className='gradient__text'>Let’s Build Something amazing with GPT-3 OpenAI</h1>
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+        <div className='dpt3__header-content_input'>
+          <input type='email' placeholder='Your Email Address' />
+          <button type='button'>Get Started </button>
+        </div>
+        <div className='gpt3__header-container__people'>
+          <img src={People} alt='people'/>
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+      </div>
+        <div className='gpt3__header-image'>
+          <img src={Ai} alt='ai image' />
+        </div>
     </div>
   )
 }
