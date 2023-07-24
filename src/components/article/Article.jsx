e3a530a96f28c455e5549145d90cc428840f7ce3
SaHaD_A-T <sahadali696@gmail.com>
2023-07-24 19:09:55 +0530

    blog section half completed

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import './article.css';
 
-function Article() {
+const Article = ({imgUrl, date, title}) => {
   return (
-    <div>
-      Article
+    <div className='gpt3__blog-container_article'>
+      <div className='gpt3__blog-container_article-image'>
+        <img  src={imgUrl} alt='blog images'/>
+      </div>
+      <div className='gpt3__blog-container_article-content'>
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <div>
+          <p>
+            Read Full Article
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
 
 export default Article
+
